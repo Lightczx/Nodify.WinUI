b7792bc979bef722b4d7b5f28348fc7de277fc94
DismissedLight <1686188646@qq.com>
2026-02-28 09:43:09 +0800

    init commit

---
+++ Nodify.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.UI.Xaml;
+
+namespace Nodify.WinUI
+{
+public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
